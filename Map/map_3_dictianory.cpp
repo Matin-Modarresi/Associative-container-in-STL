eac2802f121c0ae8896c66a266955901f29b9bc7
Matin-Modarresi <58595108+Matin-Modarresi@users.noreply.github.com>
2020-10-22 23:48:28 +0330

    Dictionary with multimap

---
+++ Map/map_3_dictianory.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <iomanip>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	multimap<string,string> dict; // elemant is ordered and we have several key 
+	
+	dict.insert( {
+	             {"day","tag"} , {"car","Auto"} , {"trait","Merkmal"} , {"strange","fremed"} , 
+               	 {"smart","elegant"} , {"smart" , "raffiniert"} , {"smart","klug"},
+               	 {"clever","raffiniert"} , {"strang","seltsam"}
+	           } );
+	           
+	           /*
+			     English   German
+                 car       Auto
+                 clever    raffiniert
+                 day       tag
+                 smart     elegant
+                 smart     raffiniert
+                 smart     klug
+                 strang    seltsam
+                 strange   fremed
+                 trait     Merkmal
+			   
+			   */
+	           
+	    
+	cout << left << setw(10) << "English" << "German" << endl;
+	
+	for(const auto& elem : dict)
+	   cout << setw(10) << elem.first << elem.second << "\n";
+	   
+	   cout << "\n";
+	   
+  string word("smart");
+  cout << word << ":\n";
+  
+  for(auto pos = dict.lower_bound(word) ; pos != dict.upper_bound(word) ; pos++)
+      cout << pos->second << endl;
+      
+      
+      /*
+	    smart:
+        elegant
+        raffiniert
+        klug
+		
+	*/
+      
+      cout << endl;
+      
+   word = "raffiniert";   
+  for(const auto pos:dict)
+    if(pos.second == word)
+      cout << pos.first << "\n"; 
+	
+	/*
+	clever
+    smart
+	*/
+	  	
+}
